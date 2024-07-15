98e4b1b5398d392bf5baf9a67ddb0f0d66cf129b
sh kwon <harvy1070@gmail.com>
2024-07-15 17:28:43 +0900

    blog database

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('test1/', views.test1),
+    path('test2/<int:no>', views.test2),
+    path('test3/<year>/<month>/<day>/', views.test3)
 ]
+
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
+    "music",
 ]
 
 MIDDLEWARE = [
@@ -77,7 +78,7 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "chinook.db",
     }
 }
 
