78e7bbea56948e6c8e7b73bfb2da6bc65f6ff387
sh kwon <harvy1070@gmail.com>
2024-07-23 20:37:32 +0900

    Delete

--- mysite/book/urls.py
+++ mysite/book/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse, reverse_lazy
 from django.views.generic import *
 from .models import Book
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('detail/<pk>', DetailView.as_view(model=Book), name='detail'),
     path('create/', CreateView.as_view(model=Book, fields='__all__'), name='create'),
     path('update/<pk>', UpdateView.as_view(model=Book, fields='__all__'), name='update'),
+    path('delete/<pk>', DeleteView.as_view(model=Book, success_url=reverse_lazy('book:list')), name='delete'),
 ]
