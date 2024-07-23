5495d97977a17408568d7d2b9338702f78e4728d
sh kwon <harvy1070@gmail.com>
2024-07-23 20:26:50 +0900

    create_modify

--- mysite/book/urls.py
+++ mysite/book/urls.py
@@ -7,4 +7,6 @@ app_name = 'book'
 urlpatterns = [
     path('', ListView.as_view(model=Book), name='list'),
     path('detail/<pk>', DetailView.as_view(model=Book), name='detail'),
+    path('create/', CreateView.as_view(model=Book, fields='__all__'), name='create'),
+    path('update/<pk>', UpdateView.as_view(model=Book, fields='__all__'), name='update'),
 ]
