50bb874ccff836a8cbbb4a4e9cbe71f8354b3df9
sh kwon <harvy1070@gmail.com>
2024-07-24 18:43:24 +0900

    admin

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -3,8 +3,13 @@ from .models import *
 
 
 # Register your models here.
-admin.site.register(Post)
+# admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(User)
 admin.site.register(Profile)
 admin.site.register(Tag)
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post, PostAdmin)
