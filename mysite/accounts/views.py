af99e7a424df24b7837adde9979c13970ad9ae14
sh kwon <harvy1070@gmail.com>
2024-07-25 21:36:03 +0900

    password_change

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
 from .forms import SignupForm
+from django.contrib.auth.views import PasswordChangeView
+from django.urls import reverse_lazy
+from django.contrib import messages
 
+# Login
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
@@ -12,3 +16,11 @@ def signup(request):
     else:form = SignupForm()
     
     return render(request, 'registration/signup.html', {'form':form})
+
+# PasswordChange
+class MypasswordChangeView(PasswordChangeView):
+    success_url = reverse_lazy('profile')
+    
+    def form_valid(self, form):
+        messages.info(self.request, '암호 변경 완료')
+        return super().form_valid(form)
