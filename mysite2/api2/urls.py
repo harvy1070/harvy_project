14fbe6586b7d82383f91ba5e06af759061ed3566
sh kwon <harvy1070@gmail.com>
2024-07-27 20:59:51 +0900

    좋아요 기능, URL Path로 직접 연결

--- mysite2/api2/urls.py
+++ mysite2/api2/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import UserViewSet
+from .views import UserViewSet, PostViewSet, CommentViewSet
+from . import views
 
-router = routers.DefaultRouter()
-router.register('users', UserViewSet)
+# Router을 활용한 Path 정의
+# router = routers.DefaultRouter()
+# router.register('users', UserViewSet)
+# router.register('post', PostViewSet)
+# router.register('comment', CommentViewSet)
+
+# URL Path 직접 정의
 urlpatterns = [
-    path('', include(router.urls)),
+    path('post/', views.PostListAPIView.as_view(), name="post-list"),
+    path('post/<int:pk>/', views.PostRetrieveAPIView.as_view(), name="post-detail"),
+    path('comment/', views.CommentCreateAPIView.as_view(), name="comment-list"),
+    path('post/<int:pk>/like/', views.PostLikeAPIView.as_view(), name="post-like"),
 ]
--- mysite2/api2/views.py
+++ mysite2/api2/views.py
@@ -1,7 +1,56 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from .serializers import UserSerializer
+from .serializers import UserSerializer, PostSerializer, CommentSerializer
+from blog.models import Post, Comment
+from rest_framework.generics import *
+from .serializers import *
+from rest_framework.response import Response
 
+# Router을 활용한 Path 정의
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    
+# URL Path 직접 정의
+class PostListAPIView(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostListSerializer
+    
+class PostRetrieveAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostRetrieveSerializer
+    
+class CommentCreateAPIView(CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    
+class PostLikeAPIView(UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostListSerializer
+    
+    def update(self, request, *args, **kwargs):
+        # 부분 업데이트 허용 금지
+        partial = kwargs.pop('partial', False)
+        # 요청 객체 가져오기
+        instance = self.get_object()
+        # like 필드값 1 증가
+        data = {'like':instance.like + 1}
+        # 새 데이터와 결합
+        serializer = self.get_serializer(instance, data=data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        # 객체 업데이트
+        self.perform_update(serializer)
+        
+        # 미리 가져온 객체 캐시 지우기(일관성 유지)
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+        return Response(serializer.data)
+            
