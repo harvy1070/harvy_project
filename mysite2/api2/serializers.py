81d8dfac77a32853a5bfc4c547fd5136a12f9817
sh kwon <harvy1070@gmail.com>
2024-07-29 18:47:18 +0900

    Customizing'

--- mysite2/api2/serializers.py
+++ mysite2/api2/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from blog.models import Post, Comment
+from blog.models import Post, Comment, Category, Tag
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -19,11 +19,50 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = '__all__'
         
 class PostListSerializer(serializers.ModelSerializer):
+    category = serializers.CharField(source='category.name')
     class Meta:
         model = Post
         fields = ['id', 'title', 'image', 'like', 'category']
-        
+
 class PostRetrieveSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+    tags = serializers.StringRelatedField(many=True)
     class Meta:
         model = Post
         exclude = ['create_dt']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['name']
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['name']
+
+# Category, Tag 기본 버전
+# class CateTagSerializer(serializers.Serializer):
+#     cateList = CategorySerializer(many=True)
+#     tagList = TagSerializer(many=True)
+    
+# Category, Tag 커스터마이징
+class CateTagSerializer(serializers.Serializer):
+    cateList = serializers.ListField(child=serializers.CharField())
+    tagList = serializers.ListField(child=serializers.CharField())
+    
+class PostSerializerSub(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title']
+
+class CommentSerializerSub(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'content', 'update_dt']
+
+class PostSerializerDetail(serializers.Serializer):
+    post = PostRetrieveSerializer()
+    prevPost = PostSerializerSub()
+    nextPost = PostSerializerSub()
+    commentList = CommentSerializerSub(many=True)
--- mysite2/api2/views.py
+++ mysite2/api2/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from .serializers import UserSerializer, PostSerializer, CommentSerializer
+from .serializers import *
 from blog.models import Post, Comment
 from rest_framework.generics import *
-from .serializers import *
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
+from collections import OrderedDict
 
 # Router을 활용한 Path 정의
 class UserViewSet(viewsets.ModelViewSet):
@@ -18,15 +20,52 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    
+
 # URL Path 직접 정의
+class PostPageNumberPagination(PageNumberPagination):
+    page_size = 3
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('pageCnt', self.page.paginator.num_pages),
+            ('curPage', self.page.number),
+            ('postList', data),
+        ]))
+    
 class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
+    pagination_class = PostPageNumberPagination
+    def get_serializer_context(self):
+        return {
+            'request' : None,
+            'format' : self.format_kwarg,
+            'view' : self
+        }
     
 class PostRetrieveAPIView(RetrieveAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostRetrieveSerializer
+    serializer_class = PostSerializerDetail
+    
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        prevInstance, nextInstance = get_prev_next(instance)
+        commentList = instance.comment_set.all()
+        data = {
+            'post':instance,
+            'prevPost':prevInstance,
+            'nextPost':nextInstance,
+            'commentList':commentList
+        }
+        serializer = self.get_serializer(instance=data)
+        
+        return Response(serializer.data)
+
+    def get_serializer_context(self):
+        return {
+            'request':None,
+            'format':self.format_kwarg,
+            'view':self
+        }
     
 class CommentCreateAPIView(CreateAPIView):
     queryset = Comment.objects.all()
@@ -53,4 +92,36 @@ class PostLikeAPIView(UpdateAPIView):
         if getattr(instance, '_prefetched_objects_cache', None):
             instance._prefetched_objects_cache = {}
         return Response(data['like'])
-            
+    
+class CateTagAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        cateList = Category.objects.all()
+        tagList = Tag.objects.all()
+        data = {
+            'cateList' : cateList,
+            'tagList' : tagList
+        }
+        serializer = CateTagSerializer(instance=data)
+        return Response(serializer.data)
+    
+class PostLikeAPIView(GenericAPIView):
+    queryset = Post.objects.all()
+    def get(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.like += 1
+        instance.save()
+        return Response(instance.like)
+
+def get_prev_next(instance):
+    try:
+        prev = instance.get_previous_by_update_dt()
+    except instance.DoesNotExist:
+        prev = None
+    
+    try:
+        next_ = instance.get_next_by_update_dt()
+    except instance.DoseNotExist:
+        next_ = None
+    
+    return prev, next_
+        
